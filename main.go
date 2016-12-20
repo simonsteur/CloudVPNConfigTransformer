bc3a7d25f5dac19a9bfbe96d7aeab119beb6986d
simons <simon@sinch.com>
2016-12-20 14:43:33 +0100

    update

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "cvct"
-	app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
-	app.Version = 0.1
-	app.Action = func(c *cli.Context) error {
 
+    var os string
+
+	app := cli.NewApp()
+    app.Name = "cvct"
+    app.Version = 0.1
+    app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
+
+    app.Flags = []cli.Flag {
+        cli.StringFlag{
+            Name: "netscreen",
+            value: "",
+            Usage: "transform netscreen configuration",
+            Destination: &os,
+        }
+    }
+    app.Action = func(c *cli.Context) error {
+    
 	}
 }
