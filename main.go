78819a441c33bf57c3fe006d3ac8929e687fadaa
simons <simon@sinch.com>
2016-12-20 15:19:40 +0100

    update

--- main.go
+++ main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"sort"
+
 	"github.com/urfave/cli"
 )
 
 func main() {
 
-    var os string
+	var OS string
 
 	app := cli.NewApp()
-    app.Name = "cvct"
-    app.Version = 0.1
-    app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
+	app.Name = "cvct"
+	app.Version = "0.1"
+	app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
 
-    app.Flags = []cli.Flag {
-        cli.StringFlag{
-            Name: "netscreen",
-            value: "",
-            Usage: "transform netscreen configuration",
-            Destination: &os,
-        }
-    }
-    app.Action = func(c *cli.Context) error {
-    
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "netscreen",
+			Usage:       "transform Juniper Netscreen configuration",
+			Destination: &OS,
+		},
+		cli.StringFlag{
+			Name:        "junos",
+			Usage:       "transform Juniper JunOS configuration",
+			Destination: &OS,
+		},
 	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+
+	app.Action = func(c *cli.Context) error {
+		if OS == "netscreen" {
+			fmt.Println("this would be the netscreen stuff yea")
+		}
+		if OS == "junos" {
+			fmt.Println("this would be the junos stuff yea")
+
+		}
+		return nil
+	}
+
+	app.Run(os.Args)
 }
