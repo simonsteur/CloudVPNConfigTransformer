7b796d488db3b185ed1af290f35bdb6f453a2799
simons <simon@sinch.com>
2016-12-20 14:37:48 +0100

    update

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/urfave/cli"
+)
 
+func main() {
+	app := cli.NewApp()
+	app.Name = "cvct"
+	app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
+	app.Version = 0.1
+	app.Action = func(c *cli.Context) error {
+
+	}
 }
