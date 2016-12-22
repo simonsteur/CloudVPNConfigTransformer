c1c937a8dabea12af0557a220fb5f50e9c3f83ae
simons <simon@sinch.com>
2016-12-22 10:41:15 +0100

    update

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ import (
 
 func main() {
 
-	var OS string
+	var netscreen bool
+	var junos bool
 
 	app := cli.NewApp()
 	app.Name = "cvct"
@@ -18,25 +19,29 @@ func main() {
 	app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
 
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		cli.BoolFlag{
 			Name:        "netscreen",
 			Usage:       "transform Juniper Netscreen configuration",
-			Destination: &OS,
+			Destination: &netscreen,
 		},
-		cli.StringFlag{
+		cli.BoolFlag{
 			Name:        "junos",
 			Usage:       "transform Juniper JunOS configuration",
-			Destination: &OS,
+			Destination: &junos,
 		},
 	}
 
 	sort.Sort(cli.FlagsByName(app.Flags))
 
 	app.Action = func(c *cli.Context) error {
-		if OS == "netscreen" {
+		if junos == true && netscreen == true {
+			fmt.Println("please only select one language")
+			os.Exit(1)
+		}
+		if netscreen == true {
 			fmt.Println("this would be the netscreen stuff yea")
 		}
-		if OS == "junos" {
+		if junos == true {
 			fmt.Println("this would be the junos stuff yea")
 
 		}
