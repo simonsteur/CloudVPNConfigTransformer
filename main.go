4642b6718c7066163720299bdf19187306200035
simons <simon@sinch.com>
2017-01-02 16:04:29 +0100

    update

--- main.go
+++ main.go
@@ -1,51 +1,85 @@
 package main
 
 import (
-	"fmt"
 	"os"
-	"sort"
 
 	"github.com/urfave/cli"
 )
 
-func main() {
+var (
+	aws   bool
+	azure bool
 
-	var netscreen bool
-	var junos bool
+	file string
+	t1   string
+	t2   string
+	zone string
+	cidr string
+)
+
+func main() {
 
 	app := cli.NewApp()
 	app.Name = "cvct"
 	app.Version = "0.1"
 	app.Usage = "transform generated vpn configuration from cloud providers to represent your environment"
+	app.EnableBashCompletion = true
 
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:        "netscreen",
-			Usage:       "transform Juniper Netscreen configuration",
-			Destination: &netscreen,
+	app.Commands = []cli.Command{
+		{
+			Name:  "netscreen",
+			Usage: "use when dealing with netscreen configuration",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:        "aws",
+					Usage:       "use when dealing with aws generated configuration",
+					Destination: &aws,
+				},
+				cli.StringFlag{
+					Name:        "file, f",
+					Usage:       "use to specify the location of the file holding the generated vpn configuration",
+					Destination: &file,
+				},
+				cli.StringFlag{
+					Name:        "tunnel1, t1",
+					Usage:       "use to specify which tunnel interface should be used for Tunnel 1",
+					Destination: &t1,
+				},
+				cli.StringFlag{
+					Name:        "tunnel2, t2",
+					Usage:       "use to specify which tunnel interface should be used for Tunnel 2",
+					Destination: &t2,
+				},
+				cli.StringFlag{
+					Name:        "zone, z",
+					Usage:       "use to specify which zone these interfaces should be bound to",
+					Destination: &zone,
+				},
+				cli.StringFlag{
+					Name:        "outgoing_interface, outgoingif, oif",
+					Usage:       "use to specify the outgoing interface for the vpn",
+					Destination: &zone,
+				},
+				cli.StringFlag{
+					Name:        "cidr",
+					Usage:       "use to specify the cidr network address to use for routing over the vpn tunnel",
+					Destination: &cidr,
+				},
+			},
+			Action: doNetscreen,
 		},
-		cli.BoolFlag{
-			Name:        "junos",
-			Usage:       "transform Juniper JunOS configuration",
-			Destination: &junos,
+		{
+			Name:  "junos",
+			Usage: "use when dealing with junos configuration",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:        "aws",
+					Usage:       "use when dealing with aws generated configuration",
+					Destination: &aws,
+				},
+			},
+			Action: doJunos,
 		},
-	}
-
-	sort.Sort(cli.FlagsByName(app.Flags))
-
-	app.Action = func(c *cli.Context) error {
-		if junos == true && netscreen == true {
-			fmt.Println("please only select one language")
-			os.Exit(1)
-		}
-		if netscreen == true {
-			fmt.Println("this would be the netscreen stuff yea")
-		}
-		if junos == true {
-			fmt.Println("this would be the junos stuff yea")
-
-		}
-		return nil
 	}
 
 	app.Run(os.Args)
